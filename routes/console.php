fe780f32def346658594fcd05f1ce6c67c2b129e
ayham2525 <ayham252@gmail.com>
2025-08-16 13:52:52 +0400

    test cron

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,5 @@ Artisan::command('players:update-status', function () {
     $this->info('Statuses updated.');
 })->purpose('Update player statuses');
 
-Schedule::command(UpdatePlayerStatuses::class)->hourlyAt(50);
+Schedule::command('players:update-status')->hourlyAt(50);
 
